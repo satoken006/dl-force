7d0d688e725dfeb8e581feff5352cd4bc60aa901
satoken006 <seat.soil.74196901@gmail.com>
2019-07-31 05:17:02 +0900

    エッジ情報をJSONに保存

--- RepositoryParser.py
+++ RepositoryParser.py
@@ -30,6 +30,7 @@ class RepositoryParser:
 
     def append_edge(self, _soup):
         author_list = self.get_author_list(_soup)
+
         main_author = author_list[0].get_text(" ", strip=True)
         main_author = self.get_nickname_from_hashmap(main_author)
         main_author_id = -1;
@@ -47,8 +48,9 @@ class RepositoryParser:
                 break
 
         if not author_exists:
+            main_author_id = self.authorID
             author = {
-                "id": self.authorID, 
+                "id": main_author_id, 
                 "label": main_author,
                 "r": 1
             }
@@ -57,10 +59,10 @@ class RepositoryParser:
 
         for i in range(1, len(author_list)):
             co_author = author_list[i].get_text(" ", strip=True)
-            co_author = self.get_nickname_from_hashmap(main_author)
+            co_author = self.get_nickname_from_hashmap(co_author)
             co_author_id = -1;
 
-            if  co_author == None:
+            if co_author == None:
                 return
 
             co_author_exists = False
@@ -73,50 +75,37 @@ class RepositoryParser:
                     break
 
             if not co_author_exists:
+                co_author_id = self.authorID
                 co_author = {
-                    "id": self.authorID, 
+                    "id": co_author_id, 
                     "label": co_author,
-                    "r": 1
+                    "r": 0
                 }
                 self.authors.append(co_author)
                 self.authorID += 1
 
-        # for i in range(1, len(author_list)):
-        #     co_author = author_list[i].get_text(" ", strip=True)
-        #     co_author = self.get_nickname_from_hashmap(co_author)
-        #     co_author_id = -1
+            print(str(co_author_id) +"-->"+ str(main_author_id))
 
-        #     if co_author == None or co_author == "PDF":
-        #         return
 
-        #     for i in range(len(self.authors)):
-        #         if co_author in self.authors[i].values():
-        #             co_author_id = self.authors[i]["id"]
+            joint_works_exists = False
 
-        #     print(co_author + " --> " + main_author)
+            for i in range(len(self.joint_works)):
+                values = list(self.joint_works[i].values())
 
-        #     joint_works_exists = False
+                if (main_author_id == values[0] and co_author_id == values[1]) or (main_author_id == values[1] and co_author_id == values[0]):
+                    # weight +1 at current LINK
+                    self.joint_works[i]["weight"] += 1
+                    joint_works_exists = True
+                    break
 
-        #     for i in range(len(self.joint_works)):
-        #         values = list(self.joint_works[i].values())
-
-        #         if (main_author_id == values[0] and co_author_id == values[1]) or (main_author_id == values[1] and co_author_id == values[0]):
-        #             # weight + 1 to current LINK
-        #             self.joint_works[i]["weight"] += 1
-        #             joint_works_exists = True
-        #             break
-
-        #     if not joint_works_exists:
-        #         # add new element LINK
-        #         joint_work = {
-        #             "source": co_author_id,
-        #             "target": main_author_id,
-        #             "weight": 1
-        #         }
-        #         self.joint_works.append(joint_work)
-
-        #     print(self.authors)
-        #     print(self.joint_works)
+            if not joint_works_exists:
+                # add new element LINK
+                joint_work = {
+                    "source": co_author_id,
+                    "target": main_author_id,
+                    "weight": 1
+                }
+                self.joint_works.append(joint_work)
 
     # Get result of parsing
     def getResult(self):
