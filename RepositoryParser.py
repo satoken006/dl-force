a4a1fda343c771ee1bb7957716e031498bf4b324
satoken006 <seat.soil.74196901@gmail.com>
2019-07-31 04:37:20 +0900

    共著ペアを取り出した時点で共著者のデータを追加

--- RepositoryParser.py
+++ RepositoryParser.py
@@ -6,6 +6,7 @@ class RepositoryParser:
     def __init__(self):
         self.authorID = 0
         self.authors = []
+        self.joint_works = []
 
     # Get author list from HTML
     def get_author_list(self, soup):
@@ -31,43 +32,97 @@ class RepositoryParser:
         author_list = self.get_author_list(_soup)
         main_author = author_list[0].get_text(" ", strip=True)
         main_author = self.get_nickname_from_hashmap(main_author)
+        main_author_id = -1;
 
         if main_author == None:
             return
-        else:
-            author_exists = False
 
-            for i in range(len(self.authors)):
-                # print(self.authors[i])
-                if main_author in self.authors[i].values():
-                    self.authors[i]["r"] += 1
-                    author_exists = True
+        author_exists = False
 
-            if not author_exists:
-                author = {
-                    "id": self.authorID, 
-                    "label": main_author,
-                    "r": 1
-                }
-                self.authors.append(author)
-                self.authorID += 1
+        for i in range(len(self.authors)):
+            if main_author in self.authors[i].values():
+                self.authors[i]["r"] += 1
+                main_author_id = self.authors[i]["id"]
+                author_exists = True
+                break
 
-        print(self.authors)
+        if not author_exists:
+            author = {
+                "id": self.authorID, 
+                "label": main_author,
+                "r": 1
+            }
+            self.authors.append(author)
+            self.authorID += 1
 
         for i in range(1, len(author_list)):
             co_author = author_list[i].get_text(" ", strip=True)
-            co_author = self.get_nickname_from_hashmap(co_author)
+            co_author = self.get_nickname_from_hashmap(main_author)
+            co_author_id = -1;
 
-            if co_author == None or co_author == "PDF":
+            if  co_author == None:
                 return
 
-            print(co_author + " --> " + main_author)
-            # _list.append([co_author, main_author])
+            co_author_exists = False
+
+            for i in range(len(self.authors)):
+                if co_author in self.authors[i].values():
+                    # self.authors[i]["r"] += 1
+                    co_author_id = self.authors[i]["id"]
+                    co_author_exists = True
+                    break
+
+            if not co_author_exists:
+                co_author = {
+                    "id": self.authorID, 
+                    "label": co_author,
+                    "r": 1
+                }
+                self.authors.append(co_author)
+                self.authorID += 1
+
+        # for i in range(1, len(author_list)):
+        #     co_author = author_list[i].get_text(" ", strip=True)
+        #     co_author = self.get_nickname_from_hashmap(co_author)
+        #     co_author_id = -1
+
+        #     if co_author == None or co_author == "PDF":
+        #         return
+
+        #     for i in range(len(self.authors)):
+        #         if co_author in self.authors[i].values():
+        #             co_author_id = self.authors[i]["id"]
+
+        #     print(co_author + " --> " + main_author)
+
+        #     joint_works_exists = False
+
+        #     for i in range(len(self.joint_works)):
+        #         values = list(self.joint_works[i].values())
+
+        #         if (main_author_id == values[0] and co_author_id == values[1]) or (main_author_id == values[1] and co_author_id == values[0]):
+        #             # weight + 1 to current LINK
+        #             self.joint_works[i]["weight"] += 1
+        #             joint_works_exists = True
+        #             break
+
+        #     if not joint_works_exists:
+        #         # add new element LINK
+        #         joint_work = {
+        #             "source": co_author_id,
+        #             "target": main_author_id,
+        #             "weight": 1
+        #         }
+        #         self.joint_works.append(joint_work)
+
+        #     print(self.authors)
+        #     print(self.joint_works)
 
     # Get result of parsing
     def getResult(self):
         resultJSON = {
-            "nodes": self.authors
+            "nodes": self.authors,
+            "links": self.joint_works
         }
 
         return resultJSON
